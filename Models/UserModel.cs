9408fdec4544a8c968818f81b703a8f3473c6291
Mariusz Strużyk <mariusz@infospec.pl>
2023-01-27 13:36:52 +0100

    Version 1.0

---
+++ Models/UserModel.cs
@@ -0,0 +1,9 @@
+﻿namespace UploadFilesDemoApp.Models
+{
+    public class UserModel
+    {
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? FileName { get; set; }
+    }
+}
